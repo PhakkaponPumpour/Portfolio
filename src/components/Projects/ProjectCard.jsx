262cb970f79b43f382427d201d1d505dc79ed9b5
Phakkapon Pumpour <phakkaponpumpour@outlook.com>
2024-07-22 04:23:47 +0700

    update new project

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -11,7 +11,7 @@ export const ProjectCard = ({
       <img
         src={getImageUrl(imageSrc)}
         alt={`Image of ${title}`}
-        className={styles.image}
+        className="w-full h-48  object-cover "
       />
       <h3 className={styles.title}>{title}</h3>
       <p className={styles.description}>{description}</p>
